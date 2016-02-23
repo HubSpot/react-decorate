bb6cf2ffd6801f9549fa89111815031b2b7a33c1
Colby Rabideau <idontknow5691@gmail.com>
2016-02-22 19:23:32 -0500

    build stuff

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+/* eslint-disable */
+'use strict';
+
+var webpack = require('webpack')
+
+var isProd = process.env.NODE_ENV === 'production'
+var config = {
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel-loader'],
+      exclude: /node_modules/,
+    }],
+  },
+  output: {
+    library: 'ReactDecorate',
+    libraryTarget: 'umd',
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      '__DEV__': !isProd,
+    }),
+  ],
+};
+
+if (isProd) {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        screw_ie8: true,
+        warnings: false,
+      }
+    })
+  )
+}
+
+module.exports = config
