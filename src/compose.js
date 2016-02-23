eee8899bf48c590606a82c1ad6f35756e2e788ea
Colby Rabideau <crabideau@hubspot.com>
2016-02-23 00:25:18 -0500

    fix compose

--- src/compose.js
+++ src/compose.js
@@ -1,5 +1,5 @@
 import decorate from './decorate'
-import { IS_COMPOSING } from './compose'
+import { IS_COMPOSING } from './constants'
 
 export default function compose(...decorators) {
   const configs = decorators.map(decorator => decorator(IS_COMPOSING))
