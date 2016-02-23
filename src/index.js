0e6eb1c96aa0fa20a465229d96098d0ce42618a8
Colby Rabideau <idontknow5691@gmail.com>
2016-02-22 19:04:18 -0500

    index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import composeDecorators from './compose'
+import decorate from './decorate'
+import makeDecorator from './makeDecorator'
+
+export default {
+  composeDecorators,
+  decorate,
+  makeDecorator,
+}
