6e9f41dbe15056058858feb617c646ad2df7b7a2
Colby Rabideau <crabideau@hubspot.com>
2016-04-20 15:50:55 -0400

    makeDecorator tests

--- src/__tests__/makeDecorator-test.js
+++ src/__tests__/makeDecorator-test.js
@@ -1,8 +1,15 @@
 import { expect } from 'chai';
+import { COMPOSING } from '../constants';
+import { shallow } from 'enzyme';
 import makeDecorator, {
+  applyDecoratorToComponent,
   applyOptionsToDecorator,
 } from '../makeDecorator';
+import React from 'react';
 import { stub } from 'sinon';
+
+const MockComponent = () => <div />;
+MockComponent.displayName = 'MockComponent';
 
 const mockValue = 'testing';
 const mockDecorator = {
@@ -33,7 +40,27 @@ describe('makeDecorator', () => {
     expect(makeDecorator(mockConstructor)).to.be.a('function');
   });
 
+  describe('applyDecoratorToComponent', () => {
+    it('returns a component that renders the wrapped component', () => {
+      const Wrapped = applyDecoratorToComponent(mockDecorator, MockComponent);
+      const root = shallow(<Wrapped />);
+      expect(root.is(MockComponent)).to.equal(true);
+      expect(root.prop('testing')).to.equal(mockValue);
+    });
+
+    it('returns the decorator when composing', () => {
+      expect(
+        applyDecoratorToComponent(mockDecorator, COMPOSING)
+      ).to.equal(mockDecorator);
+    });
+  });
+
   describe('applyOptionsToDecorator', () => {
+    it('validates the decorator', () => {
+      expect(() => applyOptionsToDecorator(() => {})).to.throw();
+      expect(() => applyOptionsToDecorator(() => mockDecorator)).not.to.throw();
+    });
+
     it('passes options to the decorator constructor', () => {
       const constructor = stub().returns(mockDecorator);
       const mockOptions = {};
